e60c7ca5b5df67045ad8ef69e20dc43f14367824
GrPaladino <paladino.graziano@gmail.com>
2024-02-23 15:32:08 +0100

    Feat: hello php

--- result.php
+++ result.php
@@ -1,8 +1,15 @@
 <?php 
 
+// # Form Output
 $user_paragraph = $_POST["user-paragraph"];
+$user_bad_word = $_POST['bad-word'];
 
+// # Original Paragraph Length 
+$paragraph_length = strlen($user_paragraph);
 
+// * New Paragraph
+$new_paragraph = str_replace('Lorem', "***", $user_paragraph);
+$new_paragraph_length = strlen($new_paragraph);
 
 ?>
 
@@ -11,11 +18,36 @@ $user_paragraph = $_POST["user-paragraph"];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+
     <title>Document</title>
 </head>
 <body>
-    <h1>
-    <?php echo $user_paragraph ?>
-    </h1>
+    <div class="container">
+
+        <div class="my-3">
+            <h2>Il paragrafo é:</h2>
+            <p class='my-2'>
+                <?php echo $user_paragraph ?>
+            </p>
+        </div>
+        <div>
+            <h2>
+                La lunghezza del paragrafo é di <?php echo $paragraph_length ?> caratteri.
+            </h2>
+        </div>
+        <div class="my-5">
+            <h2>
+                Il nuovo paragrafo é:
+            </h2>
+            <p class="my-2">
+                <?php echo $new_paragraph ?>
+            </p>
+            <h2>
+                La lunghezza del nuovo paragrafo é di <?php echo $new_paragraph_length ?> caratteri.
+            </h2>
+        </div>
+    </div>
 </body>
 </html>
